7d52cf1114a82ed55ddeb234cb33197771174308
janakiraman.ramaiya <janakiramanss9@gmail.com>
2020-07-16 21:16:29 +0530

    Updates

---
+++ precache-manifest.cba8b14ab017a500cba5ca06baa9d021.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e00a1c22f81253348d30aeecff54fce",
+    "url": "/RoboApp/index.html"
+  },
+  {
+    "revision": "faee8867771c0dbae8cc",
+    "url": "/RoboApp/static/css/main.6ad96de2.chunk.css"
+  },
+  {
+    "revision": "56dfb2abcd5e4be32e94",
+    "url": "/RoboApp/static/js/2.8e80ef74.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RoboApp/static/js/2.8e80ef74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faee8867771c0dbae8cc",
+    "url": "/RoboApp/static/js/main.e389236e.chunk.js"
+  },
+  {
+    "revision": "be6f53e0dffbc98a1661",
+    "url": "/RoboApp/static/js/runtime-main.3aae2ca2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0aab49e5114df60da4139490ebc57132.js"
+  "/RoboApp/precache-manifest.cba8b14ab017a500cba5ca06baa9d021.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RoboApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
